3f2503b4b6cad6f5ae475427f93834d00fd9c211
benisyo <t.1112.sawada@gmail.com>
2018-06-16 18:03:45 +0900

    cpp commit

--- C++/main.cpp
+++ C++/main.cpp
@@ -3,6 +3,12 @@
 using namespace std;
 
 int main() {
+    int x;
+    string s;
+    s = "abcdef";
+    cin >> x;
+    cout << s << endl;
+    cout << x << endl;
     cout << 1 + 'a'  << endl;
     cout << "git hello!" << endl;
 }
