d1d2ad35415daad304f149b20088ed81454d8ad1
benisyo <t.1112.sawada@gmail.com>
2018-06-18 11:53:33 +0900

    testenv commit

---
+++ C++/test_env.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int a,b;
+    cin >> a >> b;
+    cout << a << " " << b << endl;
+
+    char c[10];
+    cin >> c[0];
+    cout << c[0] << endl;
+
+    vector<long long> v(100);
+    for (int i = 0; i < 100; i++)
+    {
+        v.at(i) = i;
+    }
+    cout << v.at(0) << " " << v.at(23) << " " << v.at(99) << endl;
+
+    string s;
+    cin >> s;
+    cout << s.at(0) << endl;
+
+    return 0;
+}
